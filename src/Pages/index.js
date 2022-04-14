8acc6e7ed8493a63c4f7b63922f87a7d4b6eafd8
amjad <amjedyehia1998@gmail.com>
2022-04-14 07:33:35 +0300

    implemented context api relates #14

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -3,3 +3,4 @@ export { default as Azkar } from './Azkar/Azkar';
 export { default as Zekr } from './Zekr/Zekr';
 export { default as Prayer } from './prayerPage/Prayer';
 export { default as Quran } from './quranPage/Quran';
+export { default as NotFound } from './404/404';
